a818b8869881f8927bbbd659c8386caba24049a1
lb1programmer <luke@linuxpc>
2014-10-08 21:43:00 -0700

    Cleanup a bunch of unnecessary stuff for Fx33

--- content/inspectorTweaks.js
+++ content/inspectorTweaks.js
@@ -7,7 +7,7 @@
 
 //There is no right click menu to extend in Firefox 22+ (well, in 27 there is only select-all, copy), these are workarounds:
 var dt = dt || {};
-dt.editHtml = function() {
+dt.editHtml = function() {/*
 	inspector.tweak_editHtml = function() {
 		// Running in the same context as the inspector.* elements in InspectorPanel.jsm,
 		// similar to copyInner.
@@ -24,7 +24,7 @@ dt.editHtml = function() {
 			
 			var toCopy = {src:''+this.selection.node.innerHTML, title:title};
 			window.openDialog("chrome://devtooltweaks/content/editDialog.xul",
-			"devtoolTweakEdit", "chrome,centerscreen,modal,resizable", /*args:*/
+			"devtoolTweakEdit", "chrome,centerscreen,modal,resizable", 
 			toCopy);
 			if (typeof toCopy.src=='string') {
 				this.selection.node.innerHTML = toCopy.src;
@@ -33,18 +33,8 @@ dt.editHtml = function() {
 		}
 		//like webconsole.js,      this.webConsoleClient.evaluateJS(aExecuteString, onResult);
 	};
-	/*var term = new JSTerm({
-		hudId:null,
-		createMessageNode:function() {
-			return {_outputAfterNode: null};
-		},
-		outputMessage:function(aCategory, node) {
-			return null;
-		}
-	});
-	term.execute('$0.style.display="none"',function() {});*/
 	inspector.tweak_editHtml();
-}
+*/}
 //Context menu events:
 dt.setA = function() {
 	if (!inspector.selection.isNode()) return;
@@ -149,7 +139,7 @@ dt.urlOpen = function(e) {
 	var url = dt.getStylesheetOrUrl();
 	frame.ruleview.inspector.selection.window.open(url);
 };
-dt.newRule = function(e) {
+dt.newRule = function(e) {/*
 	var selector = prompt('What selector do you want?\n For example: "#someIDelement .classinside"');
 	if (selector) {
 		var frame = document.getElementsByClassName('iframe-ruleview')[0].contentWindow;
@@ -172,7 +162,7 @@ dt.newRule = function(e) {
 				}
 			}
 		}
-	}
+	}*/
 }
 // window.inspector is documented in inspector-panel.js
 // .doc and window is inspector.xul window.
@@ -181,10 +171,10 @@ window.addEventListener('pageshow',function(evt) {
 	// Unfortunately you can't simply use evt.target.parentNode to get the document target's
 	// XUL wrapper.
 	
-	//console.log(evt.target);
+	console.log(evt.target);
 	
 	if (evt.target.baseURI.endsWith('cssruleview.xhtml')) { //css right panel has loaded:
-		var frame = document.getElementsByClassName('iframe-ruleview')[0];
+		/*var frame = document.getElementsByClassName('iframe-ruleview')[0];
 		//console.log('Ruleview found');
 		//console.log(evt);
 		//Not the old xul, it's a html we have to extend from here (Firefox 22+)
@@ -197,10 +187,10 @@ window.addEventListener('pageshow',function(evt) {
 			if (disp) { //Just let normal menu show for text input:
 				e.preventDefault();
 			}
-		}, true);
+		}, true);*/
 	}
 	function styleit() {//Optional style for inspector - not really needed in newr Fx29.
-		let frame = document.getElementById('markup-box').children[0]
+		/*let frame = document.getElementById('markup-box').children[0]
 		,	doc = frame.contentDocument
 		,	style= doc.createElement('style');
 		
@@ -211,95 +201,19 @@ window.addEventListener('pageshow',function(evt) {
 		));
 		//  ^ .highlighter.theme-selected is for Fx26, adjusting full-line spacing and selection border.
 		doc.body.appendChild(style);//what's the equivalent for old xul file?
-		
-		/*where is ruleview InplaceEditor?
-		InplaceEditor.prototype._orig_createInput = InplaceEditor.prototype._createInput;
-		InplaceEditor.prototype._createInput = function() {
-			InplaceEditor.prototype._orig_createInput.call(this);//original in this context.
-			//After creating:
-			this.input.addEventListener('click',function() {
-				console.log('inplace');
-				console.log(this.input);
-			})
-		}*/
+		*/
 	}
-	setTimeout(function(){//Why is setTimeout required? see https://bugzilla.mozilla.org/show_bug.cgi?id=994468
+	/*setTimeout(function(){//Why is setTimeout required? see https://bugzilla.mozilla.org/show_bug.cgi?id=994468
 		if (window.document === evt.target) {//Main (toplevel) frame loaded.
 			//window.inspector.on("markuploaded", styleit); Don't style this
 			//console.log('markuploaded set');
 		}
-	},10);
+	},10);*/
 	/*if (frame) {
 		frame.addEventListener('load',styleit);
 	}*/
 	//frame.contentWindow.addEventListener('load',styleit);
 });
-
-dt.lastclick = 0;//for dbl click detection
-dt.changeInlineEdit = function(e) {
-	if (+new Date() - dt.lastclick > 1000) { //Click, not dbl-click:
-		dt.lastclick = +new Date();
-		return;
-	}
-	/* Extra colorpicker Not needed in Fx 28+, just interferes popup css sometimes.
-	   Maybe later could be moved to the styles tab.
-
-	function insertAfter(referenceNode, newNode) {
-		referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-	}
-	//console.log(sel);
-	if (e.target.nodeName=='input' && e.target.classList.contains('styleinspector-propertyeditor')) {
-		var sel = e.target.value.substring(e.target.selectionStart, e.target.selectionEnd);
-		var b4  = e.target.value.substring(e.target.selectionStart-1,e.target.selectionStart);
-		if ((sel.match(/^[0-9a-f]{3,6}$/i) && b4=='#') || sel=='rgb' || sel=='rgba') {//Selected a color
-			var doc = e.target.ownerDocument
-			  , ed = e.target.inplaceEditor
-			  , overlay = doc.createElement('div')
-			  , container = doc.createElement('div')
-			  , slider = doc.createElement('div')
-			  , picker = doc.createElement('div')
-			overlay.setAttribute('class','over');
-			container.setAttribute('class','cp-default');//matching colorpicker.css
-			slider.setAttribute('class','slide');
-			picker.setAttribute('class','picker');
-			insertAfter(e.target, container);
-			container.appendChild(slider)
-			container.appendChild(picker)
-			var doc=picker.ownerDocument;
-			var style= doc.createElement('link');
-			style.setAttribute('href','chrome://devtooltweaks/content/lib/colorpicker.css');
-			style.setAttribute('rel','stylesheet');
-			doc.documentElement.appendChild(style);
-			doc.documentElement.appendChild(overlay);
-			
-			ed._fx_clear = ed._clear;
-			ed._clear = function() {//Don't clear yet...
-			}
-			ColorPicker( slider, picker, function(hex, hsv, rgb) {
-				e.target.value = hex; //TODO don't replace everything.
-				let evt = document.createEvent ("Event");
-				evt.initEvent ("input", true, false);
-				e.target.dispatchEvent(evt);//trigger inplaceeditor update
-			})
-			overlay.addEventListener('click',function() {
-				ed._clear = ed._fx_clear;//make clearable, cleanup:
-				overlay.parentNode.removeChild(overlay);
-				container.parentNode.removeChild(container);
-				e.target.focus();
-			})
-			
-			return false;
-		}
-	}*/
-	
-	//if (e.target.nodeName=='input' && e.target.inplaceEditor) {
-	//	var ed = e.target.inplaceEditor;
-	//}
-}
-//Catch click or enter on the rule edit span:
-//window.addEventListener('keypress',dt.changeInlineEdit);
-//dblclick not caught anymore, maybe something catches it before us? Mosueup works:
-//window.addEventListener('mouseup', dt.changeInlineEdit);
 
 (function(){
 	var addonprefs = Components.classes["@mozilla.org/preferences-service;1"]
@@ -311,9 +225,8 @@ dt.changeInlineEdit = function(e) {
 		//console.log('already set our own function');
 	} else {
 		LayoutHelpers.prototype.reallyscrollIntoViewIfNeeded =
-		LayoutHelpers.prototype.scrollIntoViewIfNeeded
+		LayoutHelpers.prototype.scrollIntoViewIfNeeded;
 		
-		//console.log("setting function");
 		LayoutHelpers.prototype.scrollIntoViewIfNeeded = function() {
 			if (addonprefs.getBoolPref("allowScrollInspect")) {
 				LayoutHelpers.prototype.reallyscrollIntoViewIfNeeded.apply(this, arguments);
